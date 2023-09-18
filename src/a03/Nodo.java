6f1c2c3d647abef29c3717c4dcead0c0ab9b5a11
Fernandes072 <joaofilho8274@gmail.com>
2023-09-18 03:30:47 -0300

    Modificações

--- src/a03/Nodo.java
+++ src/a03/Nodo.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Nodo {
-	
+
 	private Object dado;
 	private List<Nodo> filhos = new ArrayList<Nodo>();
 	private Nodo pai;
-	
+
 	public Nodo() {
 	}
-	
+
 	public Nodo(Object dado) {
 		this.dado = dado;
 	}
@@ -23,7 +23,7 @@ public class Nodo {
 	public List<Nodo> getFilhos() {
 		return filhos;
 	}
-	
+
 	public Nodo getPai() {
 		return pai;
 	}
@@ -36,7 +36,7 @@ public class Nodo {
 		filhos.add(filho);
 		filho.setPai(this);
 	}
-	
+
 	public Object[] pegaFilhos() {
 		Object[] filhos = new Object[this.filhos.size()];
 		int i = 0;
@@ -46,23 +46,23 @@ public class Nodo {
 		}
 		return filhos;
 	}
-	
+
 	public Object pegaPai() {
 		return pai.getDado();
 	}
-	
+
 	public boolean eRaiz() {
 		return pai == null;
 	}
-	
+
 	public boolean eFolha() {
 		return filhos.size() == 0;
 	}
-	
+
 	public int grau() {
 		return filhos.size();
 	}
-	
+
 	public int nivel() {
 		int i = 1;
 		Nodo nodo = this;
@@ -72,44 +72,79 @@ public class Nodo {
 		}
 		return i;
 	}
-	
-	public int contagem() {
-		int i = 0;
-		Nodo nodo = this;
-		while (nodo.getPai() != null) {
-			nodo = nodo.getPai();
-			i++;
-		}
-		
-		return contarNodos(nodo, i);
+
+	public int altura() {
+		return pegarAltura(this, 1, 1);
 	}
-	
+
+	private static int pegarAltura(Nodo nodo, int nivel, int fim) {
+		if (nivel > fim) {
+			fim = nivel;
+		}
+		for (Nodo filho : nodo.filhos) {
+			fim = pegarAltura(filho, nivel + 1, fim);
+		}
+		return fim;
+	}
+
+	public int contagem() {
+		//return contarNodos(raiz(this), 1); para contar todos os elementos
+		return contarNodos(this, 1); //conta os elementos do nó
+	}
+
 	private static int contarNodos(Nodo nodo, int cont) {
 		for (Nodo filho : nodo.filhos) {
-			contarNodos(filho, cont+1);
+			cont = 1 + contarNodos(filho, cont);
 		}
 		return cont;
 	}
-	
-	public Object busca(Object info) {
-		Nodo nodo = raiz(this);
-		
-		return nodo.pegaPai();
+
+	public Nodo busca(Object info) {
+		Nodo nodo = encontrarBusca(this, info, new Nodo());
+		if (nodo.dado != null) {
+			return nodo;
+		}
+		return null;
+	}
+
+	private static Nodo encontrarBusca(Nodo nodo, Object info, Nodo elemento) {
+		if (info.equals(nodo.dado)) {
+			System.out.println("achou");
+			return nodo;
+		}
+		for (Nodo filho : nodo.filhos) {
+			elemento = encontrarBusca(filho, info, elemento);
+		}
+		return elemento;
 	}
 	
+	public void remove(Nodo nodo) {
+		nodo.pai.filhos.remove(nodo);
+	}
+	
+	public List<Nodo> irmaos() {
+		List<Nodo> irmaos = new ArrayList<Nodo>();
+		for (Nodo irmao : pai.filhos) {
+			if(irmao != this) {
+				irmaos.add(irmao);
+			}
+		}
+		return irmaos;
+	}
+
 	private Nodo raiz(Nodo nodo) {
 		while (nodo.getPai() != null) {
 			nodo = nodo.getPai();
 		}
 		return nodo;
 	}
-	
+
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		prepararString(this, sb, 0);
 		return sb.toString();
 	}
-	
+
 	private static void prepararString(Nodo nodo, StringBuilder sb, int nivel) {
 		for (int i = 0; i < nivel; i++) {
 			sb.append("  ");
@@ -117,7 +152,26 @@ public class Nodo {
 		sb.append(nodo.dado.toString());
 		sb.append("\r\n");
 		for (Nodo filho : nodo.filhos) {
-			prepararString(filho, sb, nivel+1);
+			prepararString(filho, sb, nivel + 1);
+		}
+	}
+
+	private static void prepararString2(Nodo nodo, StringBuilder sb, int nivel) {
+		if (!nodo.eFolha()) {
+			sb.append("(");
+		}
+		sb.append(nodo.dado.toString());
+		if (!nodo.eFolha()) {
+			sb.append("(");
+		}
+		for (Nodo filho : nodo.filhos) {
+			prepararString2(filho, sb, nivel + 1);
+		}
+		if (!nodo.eFolha()) {
+			sb.append(")");
+		}
+		if (!nodo.eFolha()) {
+			sb.append(")");
 		}
 	}
 }
--- src/a03/Programa.java
+++ src/a03/Programa.java
@@ -1,10 +1,12 @@
 package a03;
 
+import java.util.List;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		
-		Nodo veiculo = new Nodo("Veiculo");
+
+		/*Nodo veiculo = new Nodo("Veiculo");
 
 		Nodo aereo = new Nodo("Aereo");
 		veiculo.adicionaFilho(aereo);
@@ -36,32 +38,62 @@ public class Programa {
 		aMotor.adicionaFilho(iate);
 		Nodo lancha = new Nodo("Lancha");
 		aMotor.adicionaFilho(lancha);
-		
-		System.out.println(veiculo);
-		
-		System.out.println(aviao.pegaPai());
+
+		System.out.println(veiculo);*/
+
+		/*System.out.println(aviao.pegaPai());
 		System.out.println();
-		
+
 		Object[] filhos = veiculo.pegaFilhos();
 		for (Object object : filhos) {
 			System.out.println(object);
 		}
 		System.out.println();
-		
+
 		System.out.println(veiculo.eRaiz());
 		System.out.println();
-		
+
 		System.out.println(aMotor.eFolha());
 		System.out.println();
-		
+
 		System.out.println(aereo.grau());
 		System.out.println();
-		
+
 		System.out.println(iate.nivel());
 		System.out.println();
-		
-		System.out.println(lancha.contagem());
+
+		System.out.println("Contagem: " + iate.contagem());
 		System.out.println();
+		
+		System.out.println("Busca: " + veiculo.busca("Avião"));
+		System.out.println();
+		System.out.println(veiculo.contagem());*/
+		
+		Nodo A = new Nodo("A");
+		Nodo B = new Nodo("B");
+		Nodo C = new Nodo("C");
+		Nodo D = new Nodo("D");
+		Nodo E = new Nodo("E");
+		Nodo F = new Nodo("F");
+		Nodo G = new Nodo("G");
+		Nodo H = new Nodo("H");
+		Nodo I = new Nodo("I");
+		Nodo J = new Nodo("J");
+		A.adicionaFilho(B);
+		A.adicionaFilho(C);
+		A.adicionaFilho(D);
+		B.adicionaFilho(E);
+		C.adicionaFilho(F);
+		C.adicionaFilho(G);
+		D.adicionaFilho(H);
+		H.adicionaFilho(I);
+		H.adicionaFilho(J);
+		System.out.println(A);
+		List<Nodo> irmaos = B.irmaos();
+		for (Nodo irmao : irmaos) {
+			System.out.println(irmao);
+		}
+		System.out.println(A);
 	}
 
 }
